7dff8f77df7e9370b00f145ec62a9ae8fbf5700a
Joey <413486687@qq.com>
2022-02-18 22:44:34 +0800

    Add header creation of posts to the script

--- createDateFile.sh
+++ createDateFile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 date=$(date --rfc-3339=date)
-touch ./_posts/$date-$1.md
+file="./_posts/$date-$1.md"
+touch $file
+echo "---" >> $file
+echo "layout: post" >> $file
+echo "title: " >> $file
+echo "date: $date" >> $file
+echo "categories: " >> $file
+echo "tags: " >> $file
+echo "---" >> $file
