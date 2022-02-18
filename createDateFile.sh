26343d433fd600019524d6b5384e563693b1a1c2
Joey <413486687@qq.com>
2022-02-18 22:52:58 +0800

    Optimizing header creation of posts

--- createDateFile.sh
+++ createDateFile.sh
@@ -2,11 +2,13 @@
 
 date=$(date --rfc-3339=date)
 file="./_posts/$date-$1.md"
-touch $file
-echo "---" >> $file
-echo "layout: post" >> $file
-echo "title: " >> $file
-echo "date: $date" >> $file
-echo "categories: " >> $file
-echo "tags: " >> $file
-echo "---" >> $file
+
+cat > $file << EOF
+---
+layout: post
+title: 
+date: $date
+categories:
+tags:
+---
+EOF
